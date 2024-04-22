60d042c2e8accbe39712d9d40fb3ea6b628d65ac
jacopo1010 <jaorru03@gmail.com>
2024-04-22 14:21:55 +0200

    Ancora lavoro sui comandi. IMPLEMENTARE CLASSE COMANDONONVALIDO

---
+++ DiaDia/src/it/uniroma3/diadia/comandi/ComandoGuarda.java
@@ -0,0 +1,36 @@
+package it.uniroma3.diadia.comandi;
+
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+import it.uniroma3.diadia.giocatore.Giocatore;
+
+public class ComandoGuarda implements Comando {
+
+	@Override
+	public void esegui(Partita partita) {
+	Stanza guardaStanza = partita.getStanzaCorrente();
+    System.out.println(guardaStanza.toString());
+    System.out.println("Cfu in possesso: " + partita.getGiocatore().getCfu());
+	}
+	
+
+	@Override
+	public void setParametro(String parametro) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getParametro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ DiaDia/src/it/uniroma3/diadia/comandi/ComandoNonValido.java
@@ -0,0 +1,31 @@
+package it.uniroma3.diadia.comandi;
+
+import it.uniroma3.diadia.Partita;
+
+public class ComandoNonValido implements Comando {
+
+	@Override
+	public void esegui(Partita partita) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setParametro(String parametro) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getNome() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getParametro() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
