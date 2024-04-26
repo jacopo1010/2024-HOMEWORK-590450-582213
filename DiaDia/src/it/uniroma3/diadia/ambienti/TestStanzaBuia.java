28552178382d29d962d19274aa57ec8c64181a36
Mhanz3500 <manuelmoscardi@gmail.com>
2024-04-26 19:20:15 +0200

    ee

---
+++ DiaDia/src/it/uniroma3/diadia/ambienti/TestStanzaBuia.java
@@ -0,0 +1,15 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStanzaBuia {
+	StanzaBuia test1 = new StanzaBuia("test1", "lanterna");
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
